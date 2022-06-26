2636689cdab91491c942504716ca308d5d38287e
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-26 22:14:59 +0530

    hyperlink connecting to another page at a speicific part, as well as in same page

---
+++ html/htmlDay5.java
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is our third page</h1>
+    <a href="#title1">Title1</a>
+    <a href="#title2">Title2</a>
+    <a href="#title3">Title3</a>
+    <a href="#title4">Title4</a>
+    <a href="#title5">Title5</a>
+    <a href="#title6">Title6</a>
+    <a href="#title7">Title7</a>
+
+    <!-- connecting to another page at a speicific part. -->
+    <a href="./firstProgram.html#para">First-page</a>
+    <!-- # means remains in same page -->
+    <a id="title1"></a>
+   <h2>tittle-1</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
+        iusto! Necessitatibus autem, molestiae, vero incidunt ad voluptas non similique culpa reprehenderit ex 
+        repudiandae veniam exercitationem doloribus aliquam dolorum illo assumenda accusantium, nisi veritatis ipsa impedit.
+        Architecto mollitia alias sapiente atque reiciendis quas tempore, facere facilis recusandae fuga asperiores non, enim
+        numquam assumenda amet eos, dignissimos ut?
+        Repellat tempore nemo consequatur exercitationem quia modi autem, optio ab quidem 
+        animi culpa quaerat sed saepe odit, mollitia nesciunt iste ad repudiandae, illum voluptate ex magni accusamus! 
+        Ipsa nostrum fugit sunt, dignissimos aliquam animi perspiciatis!
+        Reiciendis, dolorum quia. Porro alias quaerat ut laudantium voluptatibus!
+    </p>
+    <a id="title2"></a>
+    <h2>tittle-2</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
+        iusto! Necessitatibus autem, molestiae, vero incidunt ad voluptas non similique culpa reprehenderit ex 
+        repudiandae veniam exercitationem doloribus aliquam dolorum illo assumenda accusantium, nisi veritatis ipsa impedit.
+        Architecto mollitia alias sapiente atque reiciendis quas tempore, facere facilis recusandae fuga asperiores non, enim
+        numquam assumenda amet eos, dignissimos ut?
+        Repellat tempore nemo consequatur exercitationem quia modi autem, optio ab quidem 
+        animi culpa quaerat sed saepe odit, mollitia nesciunt iste ad repudiandae, illum voluptate ex magni accusamus! 
+        Ipsa nostrum fugit sunt, dignissimos aliquam animi perspiciatis!
+        Reiciendis, dolorum quia. Porro alias quaerat ut laudantium voluptatibus!
+    </p>
+    <a id="title3"></a>
+    <h2>tittle-3</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
+        iusto! Necessitatibus autem, molestiae, vero incidunt ad voluptas non similique culpa reprehenderit ex 
+        repudiandae veniam exercitationem doloribus aliquam dolorum illo assumenda accusantium, nisi veritatis ipsa impedit.
+        Architecto mollitia alias sapiente atque reiciendis quas tempore, facere facilis recusandae fuga asperiores non, enim
+        numquam assumenda amet eos, dignissimos ut?
+        Repellat tempore nemo consequatur exercitationem quia modi autem, optio ab quidem 
+        animi culpa quaerat sed saepe odit, mollitia nesciunt iste ad repudiandae, illum voluptate ex magni accusamus! 
+        Ipsa nostrum fugit sunt, dignissimos aliquam animi perspiciatis!
+        Reiciendis, dolorum quia. Porro alias quaerat ut laudantium voluptatibus!
+    </p>
+    <a id="title4"></a>
+    <h2>tittle-4</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
+        iusto! Necessitatibus autem, molestiae, vero incidunt ad voluptas non similique culpa reprehenderit ex 
+        repudiandae veniam exercitationem doloribus aliquam dolorum illo assumenda accusantium, nisi veritatis ipsa impedit.
+        Architecto mollitia alias sapiente atque reiciendis quas tempore, facere facilis recusandae fuga asperiores non, enim
+        numquam assumenda amet eos, dignissimos ut?
+        Repellat tempore nemo consequatur exercitationem quia modi autem, optio ab quidem 
+        animi culpa quaerat sed saepe odit, mollitia nesciunt iste ad repudiandae, illum voluptate ex magni accusamus! 
+        Ipsa nostrum fugit sunt, dignissimos aliquam animi perspiciatis!
+        Reiciendis, dolorum quia. Porro alias quaerat ut laudantium voluptatibus!
+    </p>
+    
+    <a id="title6"></a>
+    <h2>tittle-6</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
+        iusto! Necessitatibus autem, molestiae, vero incidunt ad voluptas non similique culpa reprehenderit ex 
+        repudiandae veniam exercitationem doloribus aliquam dolorum illo assumenda accusantium, nisi veritatis ipsa impedit.
+        Architecto mollitia alias sapiente atque reiciendis quas tempore, facere facilis recusandae fuga asperiores non, enim
+        numquam assumenda amet eos, dignissimos ut?
+        Repellat tempore nemo consequatur exercitationem quia modi autem, optio ab quidem 
+        animi culpa quaerat sed saepe odit, mollitia nesciunt iste ad repudiandae, illum voluptate ex magni accusamus! 
+        Ipsa nostrum fugit sunt, dignissimos aliquam animi perspiciatis!
+        Reiciendis, dolorum quia. Porro alias quaerat ut laudantium voluptatibus!
+    </p>
+    <a id="title7"></a>
+    <h2>tittle-7</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
+        iusto! Necessitatibus autem, molestiae, vero incidunt ad voluptas non similique culpa reprehenderit ex 
+        repudiandae veniam exercitationem doloribus aliquam dolorum illo assumenda accusantium, nisi veritatis ipsa impedit.
+        Architecto mollitia alias sapiente atque reiciendis quas tempore, facere facilis recusandae fuga asperiores non, enim
+        numquam assumenda amet eos, dignissimos ut?
+        Repellat tempore nemo consequatur exercitationem quia modi autem, optio ab quidem 
+        animi culpa quaerat sed saepe odit, mollitia nesciunt iste ad repudiandae, illum voluptate ex magni accusamus! 
+        Ipsa nostrum fugit sunt, dignissimos aliquam animi perspiciatis!
+        Reiciendis, dolorum quia. Porro alias quaerat ut laudantium voluptatibus!
+    </p>
+    <a href="#title1">go to top</a>
+    
+
+</body>
+</html>
